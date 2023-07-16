bd894dc59871458ad692ac4e6dd9c356491a4671
Michael Lustig <lustig@knophy.com>
2023-07-16 11:04:02 -0400

    Save audio file and playback working but not with correct transcription

--- code/TextSizeFaceDistance/TextSizeFaceDistance/ContentView.swift
+++ code/TextSizeFaceDistance/TextSizeFaceDistance/ContentView.swift
@@ -11,9 +11,18 @@ import SwiftUI
 import ARKit
 import Speech
 
+struct RecordedAudio : Identifiable {
+  let id = UUID()
+    let fileURL: URL
+    let transcription: String
+}
+
+
 struct ContentView: View {
   @State private var faceDistance: Float = 0
   @State private var previousFaceDistance: Float = 0
+  
+  @ObservedObject var recorderAndPlayer = RecorderAndPlayer()
   
   @State private var recognizedText: String = ""
   @State private var baseFontSize: Float = 72
@@ -35,11 +44,28 @@ struct ContentView: View {
       }
       .frame(width: 100, height: 100)
       
+      if recorderAndPlayer.isRecording {
+                 Button("Stop Recording", action: recorderAndPlayer.stopListeningAndSave)
+             } else {
+                 Button("Start Recording", action: recorderAndPlayer.startListening)
+             }
+
+//      Swift Compiler complaining about
+//      Cannot convert value of type '[RecordedAudio]' to expected argument type 'Binding<C>'
+//             ForEach(recorderAndPlayer.recordedAudios) { recordedAudio in
+//               VStack {
+//                   Text(recordedAudio.transcription)
+//                   Button("Play", action: {
+//                       recorderAndPlayer.playRecording(at: recordedAudio.fileURL)
+//                   })
+//               }
+//             }
+      
       Spacer()
       
       ScrollViewReader { proxy in
           ScrollView {
-              Text("Recognized Text: \(recognizedText)")
+            Text(recorderAndPlayer.recognizedText)
                   .font(.system(size: getFontSize()))
                   .id("end")
                   .onChange(of: recognizedText) { newValue in
@@ -106,6 +132,90 @@ struct ContentView: View {
         }
     }
 }
+
+
+class RecorderAndPlayer: ObservableObject {
+    private var audioEngine = AVAudioEngine()
+    private var speechRecognizer = SFSpeechRecognizer()
+    private var audioFile: AVAudioFile?
+    private var audioPlayer: AVAudioPlayer?
+    
+    @Published var recognizedText: String = ""
+    @Published var recordedAudios: [RecordedAudio] = []
+    
+    var isRecording: Bool {
+        audioEngine.isRunning
+    }
+    
+    func startListening() {
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            
+            let recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            
+            let inputNode = audioEngine.inputNode
+            
+            // Prepare the audio file
+            let fileUrl = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+            audioFile = try AVAudioFile(forWriting: fileUrl, settings: inputNode.outputFormat(forBus: 0).settings)
+            
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: inputNode.outputFormat(forBus: 0)) { [weak self] buffer, _ in
+                recognitionRequest.append(buffer)
+                
+                // Write buffer data to the audio file on a background queue
+                DispatchQueue.global(qos: .userInitiated).async {
+                    do {
+                        try self?.audioFile?.write(from: buffer)
+                    } catch {
+                        print("Failed to write buffer to file: \(error)")
+                    }
+                }
+            }
+            
+            audioEngine.prepare()
+            try audioEngine.start()
+            
+            speechRecognizer?.recognitionTask(with: recognitionRequest) { [weak self] result, error in
+                if let result = result {
+                    self?.recognizedText = result.bestTranscription.formattedString
+                } else if let error = error {
+                    print("Recognition failed: \(error)")
+                }
+            }
+        } catch {
+            print("Failed to set up speech recognition: \(error)")
+        }
+    }
+    
+    func stopListening() -> RecordedAudio {
+        audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        
+      let recordedAudio = RecordedAudio(fileURL: audioFile!.url, transcription: recognizedText)
+        
+        return recordedAudio
+    }
+        
+    func stopListeningAndSave() {
+        let recordedAudio = stopListening()
+        recordedAudios.append(recordedAudio)
+    }
+
+    
+    func playRecording() {
+        guard let audioFile = audioFile else { return }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile.url)
+            audioPlayer?.play()
+        } catch {
+            print("Failed to play audio file: \(error)")
+        }
+    }
+}
+
 
 
 struct ARViewContainer: UIViewRepresentable {
