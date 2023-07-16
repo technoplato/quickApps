10a650bf0d00bea00a5a8e856633d16cc29a5dcc
Michael Lustig <lustig@knophy.com>
2023-07-16 10:17:52 -0400

    Add speech recognition and cleanup

--- code/TextSizeFaceDistance/TextSizeFaceDistance/ContentView.swift
+++ code/TextSizeFaceDistance/TextSizeFaceDistance/ContentView.swift
@@ -9,28 +9,75 @@ import SwiftUI
 
 import SwiftUI
 import ARKit
+import Speech
 
 struct ContentView: View {
   @State private var faceDistance: Float = 0
   @State private var previousFaceDistance: Float = 0
+  @State private var recognizedText: String = ""
+  
+  private let audioEngine = AVAudioEngine()
+  private let speechRecognizer = SFSpeechRecognizer()
   
   let distanceThreshold: Float = 0.001 // Set a threshold for significant changes
-
+  
   var body: some View {
     VStack {
       ARViewContainer { newFaceDistance in
         // Only update the face distance state if the change is greater than the threshold
         if abs(newFaceDistance - previousFaceDistance) > distanceThreshold {
-            faceDistance = newFaceDistance
-            previousFaceDistance = newFaceDistance
+          faceDistance = newFaceDistance
+          previousFaceDistance = newFaceDistance
         }
       }
-      .frame(width: 100, height: 100)
+      .frame(width: 200, height: 200)
       
       Text("Face Distance: \(faceDistance)")
-        .font(.system(size: CGFloat(abs(faceDistance) * 100)))  // Multiply by 100 to convert from meters to something more suitable for a font size
-    }
+        .font(.system(size: getFontSize()))
+      
+      Text("Recognized Text: \(recognizedText)")
+              
+        .font(.system(size: getFontSize()))  
+
+    }        .onAppear(perform: startListening)
+    
   }
+  
+  
+   func getFontSize() -> CGFloat {
+     
+       return CGFloat(abs(faceDistance) * 72)
+   }
+  
+  
+  func startListening() {
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            
+            let recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+//            guard let recognitionRequest = recognitionRequest else { return }
+            
+            let inputNode = audioEngine.inputNode
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: inputNode.outputFormat(forBus: 0)) { buffer, _ in
+                recognitionRequest.append(buffer)
+            }
+            
+            audioEngine.prepare()
+            try audioEngine.start()
+            
+            speechRecognizer?.recognitionTask(with: recognitionRequest) { result, error in
+                if let result = result {
+                    self.recognizedText = result.bestTranscription.formattedString
+                } else if let error = error {
+                    print("Recognition failed: \(error)")
+                }
+            }
+        } catch {
+            print("Failed to set up speech recognition: \(error)")
+        }
+    }
 }
 
 
